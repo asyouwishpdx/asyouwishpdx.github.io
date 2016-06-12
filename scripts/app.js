67041754054882a1d63f296984cd50cb7a2cc819
lwalden <lwalden77@gmail.com>
2016-06-12 11:35:17 -0700

    site updates
    including new photos, remove old rental spaces, add home furnishings
    page.

--- scripts/app.js
+++ scripts/app.js
@@ -1,260 +1,272 @@
 ﻿(function () {
-    'use strict';
+	'use strict';
 
-    var app = angular.module('app',
-        [
-        //Angular modules
-        'ngRoute',
+	var app = angular.module('app',
+		[
+		//Angular modules
+		'ngRoute',
 
-        //Third party modules
-        'mm.foundation',
-        ]
-    );
+		//Third party modules
+		'mm.foundation',
+		]
+	);
 
-    app.run([function () {
-        $(document).foundation();
-    }]);
+	app.run([function () {
+		$(document).foundation();
+	}]);
 
-    app.config(
-        ['$routeProvider',
-            function ($routeProvider) {
-                var when = $routeProvider.when;
+	app.config(
+		['$routeProvider',
+			function ($routeProvider) {
+				var when = $routeProvider.when;
 
-                $routeProvider.when = function (path, route) {
-                    route.caseInsensitiveMatch = true;
-                    route.controllerAs = "vm";
-                    return when.apply(this, arguments);
-                }
+				$routeProvider.when = function (path, route) {
+					route.caseInsensitiveMatch = true;
+					route.controllerAs = "vm";
+					return when.apply(this, arguments);
+				}
 
-                $routeProvider.when("/home", {
-                    templateUrl: "views/home.html",
-                    controller: "HomeCtrl",
-                }).when("/about", {
-                    templateUrl: "views/about.html",
-                    controller: "AboutCtrl",
-                }).when("/spaces", {
-                    templateUrl: "views/spaces.html",
-                    controller: "SpacesCtrl",
-                }).when("/events", {
-                    templateUrl: "views/events.html",
-                    controller: "EventsCtrl",
-                }).when("/giftshop", {
-                    templateUrl: "views/giftshop.html",
-                    controller: "GiftshopCtrl",
-                }).when("/businesses", {
-                    templateUrl: "views/businesses.html",
-                    controller: "BusinessesCtrl",
-                }).when("/connect", {
-                    templateUrl: "views/connect.html",
-                    controller: "ConnectCtrl",
-                }).when("/whitefeather", {
-                    templateUrl: "views/whitefeather.html",
-                    controller: "RoomCtrl",
-                }).when("/delphinroom", {
-                    templateUrl: "views/delphinroom.html",
-                    controller: "RoomCtrl",
-                }).when("/boucherroom", {
-                    templateUrl: "views/boucherroom.html",
-                    controller: "RoomCtrl",
-                }).when("/magdaline", {
-                    templateUrl: "views/magdaline.html",
-                    controller: "RoomCtrl",
-                }).when("/joanofarc", {
-                    templateUrl: "views/joanofarc.html",
-                    controller: "RoomCtrl",
-                }).otherwise({
-                    redirectTo: "/home"
-                });
-            }
-        ]
-    );
+				$routeProvider.when("/home", {
+					templateUrl: "views/home.html",
+					controller: "HomeCtrl",
+				}).when("/about", {
+					templateUrl: "views/about.html",
+					controller: "AboutCtrl",
+				}).when("/homefurnishings", {
+					templateUrl: "views/homefurnishings.html",
+					controller: "HomeFurnishingsCtrl",
+				}).when("/rentalSpace", {
+					templateUrl: "views/rentalspace.html",
+					controller: "RentalSpaceCtrl",
+				}).when("/events", {
+					templateUrl: "views/events.html",
+					controller: "EventsCtrl",
+				}).when("/giftshop", {
+					templateUrl: "views/giftshop.html",
+					controller: "GiftshopCtrl",
+				}).when("/businesses", {
+					templateUrl: "views/businesses.html",
+					controller: "BusinessesCtrl",
+				}).when("/connect", {
+					templateUrl: "views/connect.html",
+					controller: "ConnectCtrl",
+				}).when("/delphinroom", {
+					templateUrl: "views/delphinroom.html",
+					controller: "RoomCtrl",
+				}).otherwise({
+					redirectTo: "/home"
+				});
+			}
+		]
+	);
 
-    app.controller('AboutCtrl',
-        ['$rootScope', 'slideShowService',
-            function ($rootScope, slideShowService) {
-                var vm = this;
+	app.controller('AboutCtrl',
+		['$rootScope', 'slideShowService',
+			function ($rootScope, slideShowService) {
+				var vm = this;
 
-                init();
+				init();
 
-                function changeView() {
-                    $rootScope.$emit('changeView', 'about');
-                }
+				function changeView() {
+					$rootScope.$emit('changeView', 'about');
+				}
 
-                function init() {
-                    changeView();
-                    slideShowService.initSlideShow();
-                }
-            }
-        ]
-    );
+				function init() {
+					changeView();
+					slideShowService.initSlideShow();
+				}
+			}
+		]
+	);
 
-    app.controller('BusinessesCtrl',
-        ['$rootScope',
-            function ($rootScope) {
-                var vm = this;
+	app.controller('BusinessesCtrl',
+		['$rootScope',
+			function ($rootScope) {
+				var vm = this;
 
-                init();
+				init();
 
-                function changeView() {
-                    $rootScope.$emit('changeView', 'buisnesses');
-                }
+				function changeView() {
+					$rootScope.$emit('changeView', 'buisnesses');
+				}
 
-                function init() {
-                    changeView();
-                }
-            }
-        ]
-    );
+				function init() {
+					changeView();
+				}
+			}
+		]
+	);
 
-    app.controller('ConnectCtrl',
-        ['$rootScope',
-            function ($rootScope) {
-                var vm = this;
+	app.controller('ConnectCtrl',
+		['$rootScope',
+			function ($rootScope) {
+				var vm = this;
 
-                init();
+				init();
 
-                function changeView() {
-                    $rootScope.$emit('changeView', 'connect');
-                }
+				function changeView() {
+					$rootScope.$emit('changeView', 'connect');
+				}
 
-                function init() {
-                    changeView();
-                    $(document).foundation({
-                        equalizer: {
-                            equalize_on_stack: true
-                        }
-                    });
-                }
-            }
-        ]
-    );
+				function init() {
+					changeView();
+					$(document).foundation({
+						equalizer: {
+							equalize_on_stack: true
+						}
+					});
+				}
+			}
+		]
+	);
 
-    app.controller('EventsCtrl',
-        ['$rootScope', 'slideShowService',
-            function ($rootScope, slideShowService) {
-                var vm = this;
+	app.controller('EventsCtrl',
+		['$rootScope', 'slideShowService',
+			function ($rootScope, slideShowService) {
+				var vm = this;
 
-                init();
+				init();
 
-                function changeView() {
-                    $rootScope.$emit('changeView', 'events');
-                }
+				function changeView() {
+					$rootScope.$emit('changeView', 'events');
+				}
 
-                function init() {
-                    changeView();
-                    slideShowService.initSlideShow();
-                }
-            }
-        ]
-    );
+				function init() {
+					changeView();
+					slideShowService.initSlideShow();
+				}
+			}
+		]
+	);
 
-    app.controller('GiftshopCtrl',
-        ['$rootScope', 'slideShowService',
-            function ($rootScope, slideShowService) {
-                var vm = this;
+	app.controller('GiftshopCtrl',
+		['$rootScope', 'slideShowService',
+			function ($rootScope, slideShowService) {
+				var vm = this;
 
-                init();
+				init();
 
-                function changeView() {
-                    $rootScope.$emit('changeView', 'giftshop');
-                }
+				function changeView() {
+					$rootScope.$emit('changeView', 'giftshop');
+				}
 
-                function init() {
-                    changeView();
-                    slideShowService.initSlideShow();
-                }
-            }
-        ]
-    );
+				function init() {
+					changeView();
+					slideShowService.initSlideShow();
+				}
+			}
+		]
+	);
 
-    app.controller('HomeCtrl',
-        ['$rootScope',
-            function ($rootScope) {
-                var vm = this;
+	app.controller('HomeCtrl',
+		['$rootScope',
+			function ($rootScope) {
+				var vm = this;
 
-                init();
+				init();
 
-                function changeView() {
-                    $rootScope.$emit('changeView', 'home');
-                }
+				function changeView() {
+					$rootScope.$emit('changeView', 'home');
+				}
 
-                function init() {
-                    changeView();
-                }
-            }
-        ]
-    );
+				function init() {
+					changeView();
+				}
+			}
+		]
+	);
 
-    app.controller('IndexCtrl',
-        ['$rootScope',
-            function ($rootScope) {
-                var vm = this;
+	app.controller('HomeFurnishingsCtrl',
+	['$rootScope', 'slideShowService',
+		function ($rootScope, slideShowService) {
+			var vm = this;
 
-                vm.activePage = "home";
-                $rootScope.$on('changeView', function (event, data) {
-                    vm.activePage = data;
-                });
-            }
-        ]
-    );
+			init();
 
-    app.controller('RoomCtrl',
-        ['$rootScope', 'slideShowService',
-            function ($rootScope, slideShowService) {
-                var vm = this;
+			function changeView() {
+				$rootScope.$emit('changeView', 'events');
+			}
 
-                init();
+			function init() {
+				changeView();
+				slideShowService.initSlideShow();
+			}
+		}
+	]
+);
 
-                function changeView() {
-                    $rootScope.$emit('changeView', 'room');
-                }
 
-                function init() {
-                    changeView();
-                    slideShowService.initSlideShow();
-                }
-            }
-        ]
-    );
+	app.controller('IndexCtrl',
+		['$rootScope',
+			function ($rootScope) {
+				var vm = this;
 
-    app.controller('SpacesCtrl',
-        ['$rootScope',
-            function ($rootScope) {
-                var vm = this;
+				vm.activePage = "home";
+				$rootScope.$on('changeView', function (event, data) {
+					vm.activePage = data;
+				});
+			}
+		]
+	);
 
-                init();
+	app.controller('RentalSpaceCtrl',
+		['$rootScope', 'slideShowService',
+			function ($rootScope, slideShowService) {
+				var vm = this;
 
-                function changeView() {
-                    $rootScope.$emit('changeView', 'spaces');
-                }
+				init();
 
-                function init() {
-                    changeView();
-                }
-            }
-        ]
-    );
+				function changeView() {
+					$rootScope.$emit('changeView', 'rentalSpace');
+				}
 
-    app.factory('slideShowService',
-        [
-            function () {
-                var factory = this;
+				function init() {
+					changeView();
+					slideShowService.initSlideShow();
+				}
+			}
+		]
+	);
 
-                factory.initSlideShow = initSlideShow;
+	app.controller('RoomCtrl',
+		['$rootScope', 'slideShowService',
+			function ($rootScope, slideShowService) {
+				var vm = this;
 
-                return factory;
+				init();
 
-                function initSlideShow() {
-                    angular.element(document).ready(function () {
-                        $('.single-item').slick({
-                            autoplay: true,
-                            dots: true,
-                            autoplaySpeed: 3000,
-                            speed: 800,
-                            adaptiveHeight: false,
-                        });
-                    });
-                }
-            }
-        ])
+				function changeView() {
+					$rootScope.$emit('changeView', 'room');
+				}
+
+				function init() {
+					changeView();
+					slideShowService.initSlideShow();
+				}
+			}
+		]
+	);
+
+	app.factory('slideShowService',
+		[
+			function () {
+				var factory = this;
+
+				factory.initSlideShow = initSlideShow;
+
+				return factory;
+
+				function initSlideShow() {
+					angular.element(document).ready(function () {
+						$('.single-item').slick({
+							autoplay: true,
+							dots: true,
+							autoplaySpeed: 3000,
+							speed: 800,
+							adaptiveHeight: false,
+						});
+					});
+				}
+			}
+		])
 })();
